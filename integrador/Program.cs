28c17a2f5eb9612a672b84b5f43d59522a0f455c
nfloresGLA <nflores@andreani.com>
2023-01-02 22:15:45 -0300

    initial prototype

--- integrador/Program.cs
+++ integrador/Program.cs
@@ -6,7 +6,30 @@ namespace integrador
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("initial commit - integrador");
+            string nombre, apellido, email;
+            int edad;
+
+            Console.WriteLine("¡Bienvenido al formulario de inscripcion!\n");            
+
+            Console.Write("Ingresa tu nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingresa tu apellido: ");
+            apellido = Console.ReadLine();
+
+            Console.Write("Ingresa tu edad: ");
+            edad = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingresa tu email: ");
+            email = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Los datos ingresados son:");
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Nombre: " + apellido);
+            Console.WriteLine("Nombre: " + edad);
+            Console.WriteLine("Nombre: " + email);
         }
     }
 }
